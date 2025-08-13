5dd8c9132fb7b2aa47639126a6c52d58c6ddd807
Ariq S. F. Ibrahim <34961766+Rixenses@users.noreply.github.com>
2025-08-13 17:42:46 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# EN: Example config file. Copy to config.py OR use environment variable DEEPL_API_KEY instead.
+# ID: Contoh berkas konfigurasi. Salin menjadi config.py ATAU gunakan variabel lingkungan DEEPL_API_KEY.
+
+# DEEPL_API_KEY = "your_api_key_here"
